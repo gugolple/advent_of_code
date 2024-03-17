1ffb26929a0c70c31f058b244707e570635a5436
gugolple <juanignacio.croce@gmail.com>
2024-03-17 16:23:07 +0100

    Day 18, recursion with python and dynamic objects is almost free to program

---
+++ 2020/day/18/p2.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+import sys, unittest
+import re
+
+def aux_rec(ops):
+    print(ops)
+    left = None
+    op = ops[0]
+    if type(op) is list:
+        left = aux_rec(op)
+    elif op.isdigit():
+        left = int(op)
+    else:
+        raise Exception(f"Bad data!: {op}")
+
+
+    nl = []
+    for op, right in zip(ops[1::2], ops[2::2]):
+        print(op, right)
+        rv = None
+        if type(right) is list:
+            rv = aux_rec(right)
+        else:
+            rv = int(right)
+        
+        if op == "+":
+            left += rv
+        elif op == "*":
+            nl.append(left)
+            nl.append(op)
+            left = rv
+        else:
+            raise Exception(f"Bad pair!: {op} -- {right}")
+    nl.append(left)
+    print(nl, left)
+
+    left = nl[0]
+    for op, right in zip(nl[1::2], nl[2::2]):
+        print(op, right)
+        rv = None
+        if type(right) is list:
+            rv = aux_rec(right)
+        else:
+            rv = int(right)
+        
+        if op == "*":
+            left *= rv
+        else:
+            raise Exception(f"Bad pair!: {op} -- {right}")
+    print(left)
+    return left
+
+
+def parenthesis_to_lists(ops):
+    print("--", ops)
+    tmp = [] 
+    idx = 0
+    while idx < len(ops):
+        itm = ops[idx]
+        if itm == "(":
+            sub_sec, final_idx = parenthesis_to_lists(ops[idx+1:])
+            idx += final_idx + 1
+            tmp.append(sub_sec)
+        elif itm == ")":
+            return tmp, idx
+        else:
+            tmp.append(itm)
+        idx += 1
+    return tmp, len(ops)
+
+
+def main(rows):
+    tot = 0
+    for r in rows:
+        r1 = r.replace("(", "( ")
+        r2 = r1.replace(")", " )")
+        ops = r2.split(" ")
+        recursed = parenthesis_to_lists(ops)[0]
+        ttot = aux_rec(recursed)
+        tot += ttot
+    return tot
+
+
+# To run tests: "python -m unittest -v p1.py"
+class TestAdvent(unittest.TestCase):
+
+    values = [
+            ("1 + 2 * 3 + 4 * 5 + 6", 231),
+            ("1 + (2 * 3) + (4 * (5 + 6))", 51),
+            ("2 * 3 + (4 * 5)", 46),
+            ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445),
+            ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060),
+            ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340),
+            ]
+
+    def test_basic(self):
+        tot = 0
+        print()
+        for v, r in self.values:
+            print(v)
+            self.assertEqual(main([v]), r)
+            tot += r
+        print("Full")
+        raw_vals = [i[0] for i in self.values]
+        self.assertEqual(main(raw_vals), tot)
+
+
+if __name__ == "__main__":
+    rows = sys.stdin.read().strip().split("\n")
+    print(main(rows))
