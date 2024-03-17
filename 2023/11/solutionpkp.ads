8b7059cc9c515ba34ea47737fa2766fa5fa544b2
gugolple <juanignacio.croce@gmail.com>
2024-03-17 16:23:04 +0100

    First try WOHOHO!

---
+++ 2023/11/solutionpkp.ads
@@ -0,0 +1,8 @@
+with Ada.Strings.Unbounded; use  Ada.Strings.Unbounded;
+package solutionpkp is
+	type StateMachine is (name);
+	procedure statemachine_proc(capture: in Unbounded_String);
+	procedure line_proc (capture: in Unbounded_String);
+	procedure Main;
+end solutionpkp;
+
