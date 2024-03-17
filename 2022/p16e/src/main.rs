8bf73f1ffecff1d1feac8556d8ab04da8736c679
gugolple <juanignacio.croce@gmail.com>
2024-03-17 16:23:06 +0100

    Initial valid answer

--- 2022/p16e/src/main.rs
+++ 2022/p16e/src/main.rs
@@ -27,53 +27,70 @@ fn input_to_map(input: &str) -> (HashMap<String, Vec<String>>, HashMap<String, u
     return (tree, vals);
 }
 
-fn valid_vals(dest: &Vec<String>, open: HashSet<String>) -> Vec<String> {
-    dest.iter().filter(|x| !open.contains(x.as_str())).map(|x| x.to_string()).collect()
+#[derive(PartialEq)]
+enum Action {
+    Activate(String),
+    Move(String),
 }
 
-#[derive(Clone, Debug)]
-struct RecurseResult {
-    val: u64,
-    open: HashSet<String>,
+fn recurse_options(tree: &HashMap<String, Vec<String>>, vals: &HashMap<String, u64>, open: &mut HashSet<String>, node: &str) -> Vec<Action> {
+    let mut actions: Vec<Action> = Vec::new();
+    // Just recurse
+    if vals.contains_key(node) && !open.contains(node) {
+        actions.push(Action::Activate(node.to_string()));
+    }
+    // Move
+    for n in tree[node].iter() {
+        actions.push(Action::Move(n.clone()));
+    }
+    return actions;
 }
 
-fn recurse(tree: &HashMap<String, Vec<String>>, vals: &HashMap<String, u64>, seen: &mut HashMap<(String, i64, u64), RecurseResult>, open: &mut HashSet<String>, node: &str, rem: i64, score: u64) -> RecurseResult {
+
+fn recurse(tree: &HashMap<String, Vec<String>>, vals: &HashMap<String, u64>, seen: &mut HashMap<(String, String, i64, u64), u64>, open: &mut HashSet<String>, node: (&str, &str), rem: i64, score: u64) -> u64 {
     //println!("Node: {}, D: {} S: {}", node, rem, score);
     // Rem ==> time remaining
-    let seen_tup = (node.to_string(), rem, score);
+    let seen_tup = (node.0.to_string(), node.1.to_string(), rem, score);
     if seen.contains_key(&seen_tup) {
-        return seen[&seen_tup].clone();
+        return seen[&seen_tup];
     }
     if rem == 0 {
-        return RecurseResult{val: score, open: open.clone()};
+        return score;
     } else if rem < 0 {
-        return RecurseResult{val: 0, open: HashSet::new()};
+        return 0;
     }
 
-    let mut best_score = RecurseResult{val: 0, open: HashSet::new()};
+    let mut best_score = 0;
+    let mut myscore = 0;
 
-    // Logic for taking
-    if vals.contains_key(node) && !open.contains(node) {
-        open.insert(node.to_string());
-        let my_score = vals[node] * (rem -1) as u64;
-        for n in tree[node].iter() {
-            let sc = recurse(tree, vals, seen, open, n, rem-2, score + my_score);
-            if sc.val > best_score.val {
+    for act1 in recurse_options(tree, vals, open, node.0 ) {
+        if let Action::Activate(ref n) = act1 {
+            open.insert(n.to_string());
+            myscore = myscore + vals[n] * (rem -1) as u64;
+        }
+        for act2 in recurse_options(tree, vals, open, node.1) {
+            if let Action::Activate(ref n) = act2 {
+                open.insert(n.to_string());
+                myscore = myscore + vals[n] * (rem -1) as u64;
+            }
+            let next_nodes = [&act1, &act2].iter().map(|x| match x {  Action::Activate(n) => n, Action::Move(n) => n}).map(|x| x.clone()).collect::<Vec<String>>();
+            let sc = recurse(tree, vals, seen, open, (next_nodes[0].as_str(), next_nodes[1].as_str()), rem -1, score + myscore);
+            if sc > best_score {
                 best_score = sc;
             }
+            if let Action::Activate(ref n) = act2 {
+                open.remove(n.as_str());
+                myscore = myscore - vals[n] * (rem -1) as u64;
+            }
         }
-        open.remove(node);
-    }
-
-    // Logic for walking 
-    for n in tree[node].iter() {
-        let sc = recurse(tree, vals, seen, open, n, rem-1, score);
-        if sc.val > best_score.val {
-            best_score = sc;
+        if let Action::Activate(ref n) = act1 {
+            open.remove(n.as_str());
+            myscore = myscore - vals[n] * (rem -1) as u64;
         }
     }
+    //println!("L: {} R: {} S: {}", node.0, node.1, best_score);
 
-    seen.insert(seen_tup, best_score.clone());
+    seen.insert(seen_tup, best_score);
     return best_score;
 }
 
@@ -81,12 +98,9 @@ fn process_input(input: &str) -> u64 {
     let (tree, vals) = input_to_map(input);
     println!("Tree: {:?}", tree);
     println!("Vals: {:?}", vals);
-    println!("First!");
-    let mut res =  recurse(&tree, &vals, &mut HashMap::new(), &mut HashSet::new(), "AA", 26, 0);
-    println!("Second!, first: {:?}", res);
-    let res =  recurse(&tree, &vals, &mut HashMap::new(), &mut res.open, "AA", 26, res.val);
-    println!("Recurse ret: {:?}", res);
-    return res.val;
+    let rec =  recurse(&tree, &vals, &mut HashMap::new(), &mut HashSet::new(), ("AA", "AA"), 26, 0);
+    println!("Recurse ret: {}", rec);
+    return rec;
 }
 
 fn main() -> io::Result<()> {
