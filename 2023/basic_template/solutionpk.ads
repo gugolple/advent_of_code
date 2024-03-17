63f980d71b4e88d48199e98a21861b60a2ca52f9
gugolple <juanignacio.croce@gmail.com>
2024-03-17 16:23:05 +0100

    Solved not compiling issues (again).

--- 2023/basic_template/solutionpk.ads
+++ 2023/basic_template/solutionpk.ads
@@ -1,6 +1,6 @@
 with Ada.Strings.Unbounded; use  Ada.Strings.Unbounded;
 package solutionpk is
-	type StateMachine is (line, fin);
+	type StateMachine is (name, number);
 	procedure statemachine_proc(capture: in Unbounded_String);
 	procedure line_proc (capture: in Unbounded_String);
 	procedure Main;
