415cf104ba1dae633f63bf7e639561caf051a9bf
gugolple <juanignacio.croce@gmail.com>
2024-03-17 16:23:06 +0100

    Problem 10 still not working. Converted to single loop function

--- 2021/p10/main.c
+++ 2021/p10/main.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<assert.h>
+#include<glib.h>
 
 #define CLOSE_BRAKETS_SIZE 4
 typedef enum {
@@ -18,7 +19,7 @@ CloseBrakets closeBrakets(char c) {
         case ']': return CloseSquareBraket;
         case '}': return CloseCurlyBraket;
         case '>': return CloseAngleBraket;
-        default: printf("Bad close braket\n"); exit(1); 
+        default: printf("Bad close braket\n"); assert(FALSE); 
     }
 }
 
@@ -42,27 +43,48 @@ char closedBraketPair(char c) {
     }
 }
 
-char recursion(char* str, int* pos) {
-    // Prevent overrun
-    printf("%c", str[*pos]);
-    const char mychar = str[*pos];
-    if(!isOpenBraket(mychar)) return mychar;
-    const char lim = closedBraketPair(str[*pos]);
-    while(str[*pos] != '\0' && str[++*pos] != '\0' && str[*pos] != lim) {
-        // Only allow open
-        if(isOpenBraket(str[*pos])) {
-            char res = recursion(str, pos);
-            // If return was non error
-            if (res != '\0') return res;
+char recursion(char* str, int* i) {
+    GArray* arr_stack = g_array_new(FALSE, FALSE, sizeof(char));
+    char cur_chr = '\0';
+    for (*i=0; str[*i]!='\0'; (*i)++) {
+        cur_chr = str[*i];
+        printf("::%c::\n", cur_chr);
+        if (isOpenBraket(cur_chr)) {
+            // Is open
+            g_array_append_val(arr_stack, cur_chr);
         } else {
-            // Bad exit, we found another close
-            printf("%c", str[*pos]);
-            return str[*pos];
+            // Is close
+            if (arr_stack->len > 0) {
+                // Stack not empty
+                const char match_char = g_array_index(arr_stack, char, arr_stack->len-1);
+                g_array_remove_index(arr_stack, arr_stack->len-1);
+                printf("Comp stack: %c actual: %c\n", match_char, cur_chr);
+                if (closedBraketPair(match_char) != cur_chr) {
+                    // Close bracket not match
+                    printf("eary exit!\n");
+                    break;
+                }
+            } else {
+                // Stack empty
+                printf("eary exit!\n");
+                break;
+            }
         }
     }
-    // Good exit, we found our pair
-    printf("%c", str[*pos]);
-    return '\0';
+
+    if((strlen(str)) == *i) {
+        cur_chr = '\0';
+    } else {
+        printf(" ret: %c\nStack: ", cur_chr);
+        for(int j=0; j<arr_stack->len; j++) {
+            printf("%c ", g_array_index(arr_stack, char, j));
+        }
+    }
+
+    printf("\n");
+
+    g_array_free(arr_stack, TRUE);
+    return cur_chr;
 }
 
 #define STR_MAX 1000
@@ -76,16 +98,14 @@ int main(int argc, char** argv) {
         if(strlen(str) == 0) continue;
         int pos = 0;
         char res = '\0';
-        while(str[pos] != '\0' && res == '\0') {
-            printf("\nStart pos:%d\n", pos);
-            res = recursion(str, &pos);
-            if (str[pos] != '\0' && str[pos+1] != '\0') ++pos;
-        }
-        if (res!=0) {
+        printf("\nStart pos:%d\n", pos);
+        res = recursion(str, &pos);
+        if (res!='\0') {
+            printf("The braket was: %c\n", res);
             hits[(int)closeBrakets(res)]++;
         }
-        printf("\n%lld %d %c\n", strlen(str), pos, res);
-        assert(pos <= strlen(str));
+        printf("%lld %d %c\n", strlen(str), pos, res);
+        assert(pos <= (strlen(str) +1));
         str[0] = '\0';
         scanf("%100s", str);
     }
@@ -95,18 +115,18 @@ int main(int argc, char** argv) {
         hits[(int)CloseAngleBraket] * 25137;
 
     printf("%d %d %d %d\n",
-        (int)CloseRoundBraket,
-        (int)CloseSquareBraket,
-        (int)CloseCurlyBraket,
-        (int)CloseAngleBraket
-        );
+            (int)CloseRoundBraket,
+            (int)CloseSquareBraket,
+            (int)CloseCurlyBraket,
+            (int)CloseAngleBraket
+          );
 
     printf("%d %d %d %d\n",
-        hits[(int)CloseRoundBraket],
-        hits[(int)CloseSquareBraket],
-        hits[(int)CloseCurlyBraket],
-        hits[(int)CloseAngleBraket]
-        );
+            hits[(int)CloseRoundBraket],
+            hits[(int)CloseSquareBraket],
+            hits[(int)CloseCurlyBraket],
+            hits[(int)CloseAngleBraket]
+          );
 
     printf("%d\r", total);
     return 0;
