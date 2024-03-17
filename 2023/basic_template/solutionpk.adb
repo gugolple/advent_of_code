dfed3ba13dc33d0347b8ccc66e7539da6c7acba2
gugolple <juanignacio.croce@gmail.com>
2024-03-17 16:23:04 +0100

    Added more to the basic template, the common usage pattern demands

--- 2023/basic_template/solutionpk.adb
+++ 2023/basic_template/solutionpk.adb
@@ -2,15 +2,28 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Unbounded; use  Ada.Strings.Unbounded;
 with Ada.Strings.Maps; use Ada.Strings.Maps;
 
+with Ada.Containers; use Ada.Containers;
+with Ada.Containers.Vectors;
+
 with solutionpk; use solutionpk;
 
 package body solutionpk is
+	package IntegerVector is new
+		Ada.Containers.Vectors
+			(Index_Type => Natural,
+			Element_Type => Integer 
+			);
+	use IntegerVector;
+
 	state: StateMachine;
+	
+	process_line : Unbounded_String;
+	values : IntegerVector.Vector;
 	procedure statemachine_proc(
 		capture: in Unbounded_String
 		)
 	is
-		current_number : Long_Integer := 0;
+		current_number : Integer := 0;
 		trimmed_string : Unbounded_String;
 	begin
 		trimmed_string := Trim(capture, To_Set(' '), To_Set(":;, "));
@@ -20,9 +33,11 @@ package body solutionpk is
 		end if;
 		case state is
 			when name =>
-				state := name_value;
-			when name_value =>
-				current_number := Long_Integer'Value(To_String(trimmed_string));
+				process_line := trimmed_string;
+				state := number;
+			when number =>
+				current_number := Integer'Value(To_String(trimmed_string));
+				values.append(current_number);
 		end case;
 	end statemachine_proc;
 
@@ -31,11 +46,12 @@ package body solutionpk is
 		(capture: in Unbounded_String)
 	is
 		idx,idxl  : Natural := 1;
+		Search_Set : constant Character_Set := To_Set(" ,");
 	begin
 		-- Capture destination data
 		idx := Index(
 			Source => capture,
-			Pattern => " ",
+			Set => Search_Set,
 			From => idx
 			);
 		while idx /= 0 loop
@@ -47,12 +63,13 @@ package body solutionpk is
 			idxl := idx + 1;
 			idx := Index(
 				Source => capture,
-				Pattern => " ",
+				Set => Search_Set,
 				From => idx+1
 				);
 		end loop;
 		statemachine_proc(To_Unbounded_String(Slice(capture,idxl,Length(capture))));
 	end line_proc;
+
 	procedure Main is
 		F         : File_Type;
 		File_Name : constant String := "input.txt";
@@ -61,9 +78,17 @@ package body solutionpk is
 		Open (F, In_File, File_Name);
 		while not End_Of_File (F) loop
 			-- Capture the line
+			state := name;
+			values.clear;
 			str := To_Unbounded_String(Get_Line (F));
 			Put_Line(To_String(str));
 			line_proc(str);
+
+			Put_Line("Input: " & To_String(process_line));
+			for I of values loop
+				Put(I'Image & " ");
+			end loop;
+			Put_Line("");
 		end loop;
 		Close (F);
 	end Main;
