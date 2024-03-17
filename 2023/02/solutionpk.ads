79e87e4f2ee670a50fad45b644aed4b4e3c868bf
gugolple <juanignacio.croce@gmail.com>
2024-03-17 16:23:03 +0100

    Basic version working!

---
+++ 2023/02/solutionpk.ads
@@ -0,0 +1,22 @@
+with Ada.Containers.Indefinite_Hashed_Maps;
+with Ada.Strings.Hash;
+with Ada.Strings.Bounded;
+package solutionpk is
+	package Integer_Hashed_Maps is new
+	Ada.Containers.Indefinite_Hashed_Maps
+		(Key_Type        => String,
+		Element_Type    => Integer,
+		Hash            => Ada.Strings.Hash,
+		Equivalent_Keys => "=");
+	use Integer_Hashed_Maps;
+
+	type StateMachine is (InitialString, InitialNumber, Number, Color);
+
+	procedure statemachine_proc(state: in out StateMachine; capture: in String);
+
+	procedure Main;
+private
+	M : Map;
+	MCount : Map;
+end solutionpk;
+
