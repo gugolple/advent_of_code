22a590460c4d6474184ab1137014a40520eb850d
gugolple <juanignacio.croce@gmail.com>
2024-03-17 16:23:06 +0100

    Initial configuration and test

--- 2022/p15/src/main.rs
+++ 2022/p15/src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::io::prelude::*;
 
 
-fn process_input(input: &str) -> u64 {
+fn process_input(input: &str, row: u64) -> u64 {
     for line in input.split("\n") {
         if line != "" {
             println!("Line!: {}", line);
@@ -17,7 +17,7 @@ fn main() -> io::Result<()> {
     let mut buffer = String::new();
     std::io::stdin().read_to_string(&mut buffer)?;
 
-    println!("Total: {}", process_input(&buffer));
+    println!("Total: {}", process_input(&buffer, 2000000));
 
     Ok(())
 }
@@ -30,7 +30,20 @@ mod tests {
 
     #[test]
     fn test_advent_basic() {
-        let input = "";
-        assert_eq!(process_input(input),1);
+        let input = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3";
+        assert_eq!(process_input(input, 10),26);
     }
 }
