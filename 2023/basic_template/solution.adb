9acbbb7ba43d630f9ce074c0f76ce7eb75be3977
gugolple <juanignacio.croce@gmail.com>
2025-01-04 11:12:55 +0100

    Set basic template to work with packagesy

--- 2023/basic_template/solution.adb
+++ 2023/basic_template/solution.adb
@@ -1,16 +1,6 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Characters.Handling; use  Ada.Characters.Handling;
-with Ada.Strings.Unbounded; use  Ada.Strings.Unbounded;
+with solutionpk;
 
 procedure solution is
-	F         : File_Type;
-	File_Name : constant String := "input.txt";
-	str       : Unbounded_String;
 begin
-	Open (F, In_File, File_Name);
-	while not End_Of_File (F) loop
-		str := To_Unbounded_String(Get_Line (F));
-		Put_Line (To_String(str));
-	end loop;
-	Close (F);
+	solutionpk.main;
 end solution;
