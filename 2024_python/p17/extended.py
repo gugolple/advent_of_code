0612f208942edba32ad56853543167978ec9d5c0
gugolple <juanignacio.croce@gmail.com>
2025-01-04 11:12:59 +0100

    2024 p17/extended
    There was a pattern with the numbers that was octal based, meaning
    that the search space was far smaller. I DID NOT see the patter,
    although I tested for it.

--- 2024_python/p17/extended.py
+++ 2024_python/p17/extended.py
@@ -103,35 +103,57 @@ def initialize_state(state):
         st[mapping[k]] = int(v)
     return st
 
+def initialize_state_zero():
+    st = [0] * _STATE_RSIZE
+    st[_STATE_OUT_B] = list()
+    st[_STATE_INSTF] = False
+    return st
+
 def compare_equals(l1, l2):
     for e1, e2 in zip(l1, l2):
         if e1 != e2:
             return False
     return True
 
-def search_func(st, program, v):
-    lastlenoutbuf = 0
-    st[_STATE_IDX_A] = v
-    idx = st[_STATE_INSTP]
-    while idx < len(program):
-        ins, oper = program[idx], program[idx+1]
-        st[_STATE_INSTF] = False
-        #print(ins, oper)
-        instructions[ins](st, oper)
-        # If not jumped
-        if not st[_STATE_INSTF]:
-            st[_STATE_INSTP] += 2
-        idx = st[_STATE_INSTP]
-        clob = len(st[_STATE_OUT_B])
-        if lastlenoutbuf != clob:
-            # Compare curr char
-            if program[lastlenoutbuf] != st[_STATE_OUT_B][lastlenoutbuf]:
-                return False
-            # Should only increase by 1
-            lastlenoutbuf = clob
-    if len(program) == lastlenoutbuf:
-        return True
-    return False
+#def search_func(st, program, v):
+#    lastlenoutbuf = 0
+#    st[_STATE_IDX_A] = v
+#    idx = st[_STATE_INSTP]
+#    while idx < len(program):
+#        ins, oper = program[idx], program[idx+1]
+#        st[_STATE_INSTF] = False
+#        #print(ins, oper)
+#        instructions[ins](st, oper)
+#        # If not jumped
+#        if not st[_STATE_INSTF]:
+#            st[_STATE_INSTP] += 2
+#        idx = st[_STATE_INSTP]
+#        clob = len(st[_STATE_OUT_B])
+#        if lastlenoutbuf != clob:
+#            # Compare curr char
+#            if program[lastlenoutbuf] != st[_STATE_OUT_B][lastlenoutbuf]:
+#                return False
+#            # Should only increase by 1
+#            lastlenoutbuf = clob
+#    if len(program) == lastlenoutbuf:
+#        return True
+#    return False
+
+def search_func(program, tgt, ans):
+    if tgt == []:
+        return ans
+    for iv in range(8):
+        st = initialize_state_zero()
+        a = ans << 3 | iv
+        st[_STATE_IDX_A] = a
+        for idx in range(0, len(program) -2, 2):
+            ins, oper = program[idx], program[idx+1]
+            instructions[ins](st, oper)
+        lc = st[_STATE_OUT_B]
+        if lc[0] == tgt[-1]:
+            r = search_func(program, tgt[:-1], a)
+            if r:
+                return r
 
 def print_prog_human(program):
     for idx in range(0, len(program), 2):
@@ -142,27 +164,13 @@ def entry_func(inp: str):
     tot = 0
     state, program = inp.split("\n\n")
     program = [int(i) for i in program.lstrip("Program: ").split(",")]
-    print_prog_human(program)
-    st = None
-    idx = 0
-    cb = 0
-    while True:
-        cst = initialize_state(state)
-        res = search_func(cst, program, idx)
-        if res:
-            st = cst
-            break
-        csob = cst[_STATE_OUT_B]
-        if len(csob) > cb:
-            print(idx, csob)
-            cb = len(csob)
-        idx += 1
-    print(st)
-    return ",".join([str(i) for i in st[_STATE_OUT_B]]), st, idx
+    #print_prog_human(program)
+    res = search_func(program, program, 0)
+    return res
 
 if __name__ == "__main__":
     inp_str = sys.stdin.read()
-    print(entry_func(inp_str[:-1])[2])
+    print(entry_func(inp_str[:-1]))
 
 class AllTestCase(unittest.TestCase):
     def test_example(self):
@@ -171,6 +179,5 @@ Register B: 0
 Register C: 0
 
 Program: 0,3,5,4,3,0"""
-        out, state, val_a = entry_func(inp_str)
-        self.assertEqual(out, "0,3,5,4,3,0")
-        self.assertEqual(val_a, 117440)
+        out = entry_func(inp_str)
+        self.assertEqual(out, 117440)
