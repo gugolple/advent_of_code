d3203e128ab23d9b091df78357ed4b8b5dd5f619
gugolple <juanignacio.croce@gmail.com>
2025-01-04 11:12:59 +0100

    2024 template
    Added a template folder to make quick copy and paste

---
+++ 2024_python/template/normal.py
@@ -0,0 +1,16 @@
+import unittest, sys
+
+def entry_func(inp: str):
+    tot = 0
+    print(inp)
+    return tot
+
+if __name__ == "__main__":
+    inp_str = sys.stdin.read()
+    print(entry_func(inp_str[:-1]))
+
+class AllTestCase(unittest.TestCase):
+    def test_example(self):
+        inp_str = """
+"""
+        self.assertEqual(entry_func(inp_str), -1)
