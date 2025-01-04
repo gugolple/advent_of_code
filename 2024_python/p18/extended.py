f6246b4c03bc1f5c64d5bf6297cc514364a01a79
gugolple <juanignacio.croce@gmail.com>
2025-01-04 11:12:59 +0100

    2024 p18/extended
    Now it makes sense how long it takes, just find and cache the path,
    until any block falls in the path, if that then recalculate.

--- 2024_python/p18/extended.py
+++ 2024_python/p18/extended.py
@@ -1,4 +1,5 @@
 import unittest, sys, heapq
+from collections import deque
 
 def add_pos(p1, p2):
     return tuple([i + j for i, j in zip(p1, p2)])
@@ -19,21 +20,18 @@ def print_mat_set(blocks, tgt, sv):
 
 def walk_path_dijstra(blocks, tgt):
     start_loc = (0, 0)
-    seen = set()
+    seen = dict()
     hpq = list([(0, start_loc)])
-    best = 0
     while len(hpq)>0:
         c, l = heapq.heappop(hpq)
         #print(c, l)
         # We got to the end
         if l[0] == tgt[0] and l[1] == tgt[1]:
-            if best == 0 or best > c:
-                best = c
+            seen[l] = c
+            return c, seen
         if l in seen:
             continue
-        if best != 0 and c > best:
-            continue
-        seen.add(l)
+        seen[l] = c
         #print(c, l)
         for m in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             # Next location
@@ -47,20 +45,47 @@ def walk_path_dijstra(blocks, tgt):
                 heapq.heappush(hpq, (nc, nl))
     #print()
     #print_mat_set(seen, tgt, 'O')
-    return best
+    return 0, None
+
+def get_best_path(seen, orig, tgt, l, pth=None):
+    if pth is None:
+        pth = deque()
+    if l == orig:
+        return pth
+    print(l)
+    for m in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        # Next location
+        nl = add_pos(l, m)
+        # Remove outside of our scope
+        if nl[0] < 0 or nl[1] < 0 or nl[0] > tgt[0] or nl[1] > tgt[1]:
+            continue
+        if nl in seen and (seen[nl]+1) == seen[l]:
+            pth.append(nl)
+            res = get_best_path(seen, orig, tgt, nl, pth)
+            if res is not None:
+                return pth
+            pth.pop()
+    return None
 
 def entry_func(inp, tgt):
     res = None
     blocks_list = [tuple([int(j) for j in i.split(',')]) for i in inp.split('\n')]
+    path = None
     for lim in range(len(blocks_list)):
+        lb = blocks_list[lim-1]
+        print(lim, lb)
+        if path is not None:
+            if lb not in path:
+                print("Cache!")
+                continue
         blocks = set(blocks_list[:lim])
-        #print()
-        #print_mat_set(blocks, tgt, '#')
-        tot = walk_path_dijstra(blocks, tgt)
+        tot, seen = walk_path_dijstra(blocks, tgt)
         if tot == 0:
             res = blocks_list[lim-1]
             print("fail at elem", lim-1, res)
             break
+        path = set(get_best_path(seen, (0, 0), tgt, tgt))
+        print_mat_set(path, tgt, 'O')
     return res
 
 if __name__ == "__main__":
