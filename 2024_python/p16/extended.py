16a65dd81b3bbf567cb883bb08fd2bac76663fcd
gugolple <juanignacio.croce@gmail.com>
2025-01-04 11:12:59 +0100

    2024 p16/extended
    Added caching but did not improve performance. Turning to cProfile
    for answers

--- 2024_python/p16/extended.py
+++ 2024_python/p16/extended.py
@@ -80,8 +80,15 @@ def possible_moves(mat, c, l, o):
             res.append((nc, nl, no))
     return res
 
+def apply_path_to_mat(mat, seen):
+    nm = clone_mat(mat)
+    for k, v in seen.items():
+        r, c = k
+        nm[r][c] = v
+    return nm
+
 def walk_path_dijstra(mat, start_loc_or, c=0):
-    seen = set()
+    seen = dict()
     hpq = list([(c, start_loc_or[0], start_loc_or[1])])
     best = 0
     while len(hpq)>0:
@@ -90,31 +97,75 @@ def walk_path_dijstra(mat, start_loc_or, c=0):
         if get_mat(mat, l) == 'E':
             if best == 0 or best > c:
                 best = c
-        if (l, o) in seen:
+        if (l, o) in seen and seen[(l, o)] < c:
             continue
         if best != 0 and c > best:
             continue
-        seen.add((l, o))
+        seen[(l, o)] = c
         for nc, nl, no in possible_moves(mat, c, l, o):
             heapq.heappush(hpq, (nc, nl, no))
-    return best
+    return best, seen
+
+def remove_orient_seen(seen):
+    ns = dict()
+    for k, v in seen.items():
+        p, o = k
+        if p in ns and ns[p] < v:
+            continue
+        ns[p] = v
+    return ns
+
+def get_path(mat, seen, vt=0):
+    ns = remove_orient_seen(seen)
+    path_taken = set()
+    #print_mat_pad(apply_path_to_mat(mat, ns))
+    dst_pos = find_mat(mat, 'E')
+    cp = dst_pos
+    cv = ns[dst_pos]
+    #print(ns)
+    while ns[cp] != vt:
+        path_taken.add(cp)
+        #print("GP", cp, cv)
+        nlp = []
+        nlv = 0
+        for _, nl, _ in possible_moves(mat, 0, cp, Or.N):
+            if nl not in ns:
+                continue
+            nlv = ns[nl]
+            #print("GP NL", nl, nlv)
+            if nlv == (cv-1) or nlv == (cv-1001):
+                nlp.append(nl)
+        # Should only be a single path
+        if len(nlp) > 1:
+            print(nlp)
+            exit(2)
+        cp = nlp[0]
+        cv = ns[cp]
+    path_taken.add(cp)
+    return path_taken
 
 def walk_all(mat, start_loc_or):
     cnt = 0
     seen = dict()
+    good_paths = set()
     res = set()
-    tgt = walk_path_dijstra(mat, start_loc_or)
+    tgt, ps = walk_path_dijstra(mat, start_loc_or)
+    print("Start walk all")
+    good_paths |= get_path(mat, ps, 0)
     dq = deque([(0, start_loc_or[0], start_loc_or[1])])
     while len(dq) > 0:
         c, l, o = dq.popleft()
         # If checked
-        if l in seen and seen[l] < c:
+        if l in seen and seen[l] <= c:
             continue
         seen[l] = c
-        # If not good
         cnt += 1
-        if walk_path_dijstra(mat, (l, o), c) != tgt:
-            continue
+        if l not in good_paths:
+            curpathbest, curpathseen = walk_path_dijstra(mat, (l, o), c)
+            if curpathbest != tgt:
+                continue
+            print(l, o, c)
+            good_paths |= get_path(mat, curpathseen, c)
         res.add(l)
         for nc, nl, no in possible_moves(mat, c, l, o):
             dq.append((nc, nl, no))
