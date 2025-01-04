efb528fd94bc0ec65366c6658f1cbf48d56e03de
gugolple <juanignacio.croce@gmail.com>
2025-01-04 11:12:55 +0100

    Extended! FIRST TRY ALSO WOOO!

---
+++ 2023/04/solutionpkp.ads
@@ -0,0 +1,8 @@
+with Ada.Strings.Unbounded; use  Ada.Strings.Unbounded;
+package solutionpkp is
+	type StateMachine is (InitialString, InitialNumber, Winners, Given);
+	procedure statemachine_proc(state: in out StateMachine; capture: in Unbounded_String);
+	procedure line_proc (capture: in Unbounded_String);
+	procedure Main;
+end solutionpkp;
+
