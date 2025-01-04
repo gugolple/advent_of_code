98083dd79642e7a39a3dde8eab1e06d23632e194
gugolple <juanignacio.croce@gmail.com>
2025-01-04 11:12:56 +0100

    Now working with a priority heap

--- 2023/17/solutionpk.adb
+++ 2023/17/solutionpk.adb
@@ -6,6 +6,9 @@ with Ada.Unchecked_Deallocation;
 with Ada.Containers; use Ada.Containers;
 with Ada.Containers.Vectors;
 with Ada.Containers.Ordered_Sets;
+with Ada.Containers.Unbounded_Priority_Queues;
+with Ada.Containers.Indefinite_Holders;
+with Ada.Containers.Synchronized_Queue_Interfaces;
 
 with solutionpk; use solutionpk;
 
@@ -34,13 +37,6 @@ package body solutionpk is
 		Col : Integer;
 		Row : Integer;
 	end record;
-	type Location_Access is access Location;
-	procedure Free is new Ada.Unchecked_Deallocation
-		(Object => Location, Name => Location_Access);
-	type LocationDirection is record
-		loc : Location;
-		dir : Direction;
-        end record;
 
 	function "="(Left, Right: Location) return Boolean is
 	begin
@@ -52,6 +48,8 @@ package body solutionpk is
 		return Left.row < Right.Row or (Left.row = Right.row and (Left.col < Right.Col));
 	end;
 
+	type Location_Access is access Location;
+
 	function "="(Left, Right: Location_Access) return Boolean is
 	begin
 		return Left.all = Right.all;
@@ -61,6 +59,14 @@ package body solutionpk is
 	begin
 		return Left.all < Right.all;
 	end;
+
+	type LocationDirection is record
+		loc : Location;
+		dir : Direction;
+        end record;
+
+	procedure Free is new Ada.Unchecked_Deallocation
+		(Object => Location, Name => Location_Access);
 
 	package LocationSet is 
 		new Ada.Containers.Ordered_Sets(
@@ -90,7 +96,6 @@ package body solutionpk is
 		d: Direction;
 		dirSteps: Natural;
 	end record;
-	type DijkstraStorage_Access is Access DijkstraStorage;
 
 	function "<"(Left, Right: DijkstraStorage) return Boolean is
 		res : Boolean := False;
@@ -111,6 +116,8 @@ package body solutionpk is
 		return res;
 	end;
 
+	type DijkstraStorage_Access is Access DijkstraStorage;
+
 	procedure Free is new Ada.Unchecked_Deallocation
 	      (Object => DijkstraStorage, Name => DijkstraStorage_Access);
 
@@ -124,6 +131,46 @@ package body solutionpk is
 			Element_Type => DijkstraStorage_Access
 			);
 	use DijkstraStorageSet;
+
+        package DijkstraStorage_Holder is
+            new Ada.Containers.Indefinite_Holders (
+                Element_Type => DijkstraStorage_Access
+                );
+	use DijkstraStorage_Holder;
+
+        package DijkstraStorage_Interface is
+            new Ada.Containers.Synchronized_Queue_Interfaces (
+                Element_Type => DijkstraStorage_Holder.Holder
+                );
+	use DijkstraStorage_Interface;
+
+	function GetPriority(E: DijkstraStorage_Holder.Holder) return Long_Integer is
+            MAX_SIZE : constant Long_Integer := 1000;
+            MAX_STEPS : constant Long_Integer := 10;
+            result : Long_Integer;
+	begin
+                result := Long_Integer(E.Element.Cost);
+                --result := result * MAX_SIZE + Long_Integer(E.Element.l.row);
+                --result := result * MAX_SIZE + Long_Integer(E.Element.l.col);
+                --result := result * MAX_STEPS + Long_Integer(E.Element.dirSteps);
+                --result := result * MAX_STEPS + Long_Integer(Direction'Pos(E.Element.d));
+		return result;
+	end;
+
+        function DS_Before(L, R: Long_Integer) return boolean
+        is
+        begin
+            return L<R;
+        end;
+
+        package DijkstraStorageQueue is 
+		new Ada.Containers.Unbounded_Priority_Queues(
+			Queue_Interfaces => DijkstraStorage_Interface,
+                        Queue_Priority => Long_Integer,
+                        Get_Priority => GetPriority,
+                        Before => DS_Before
+			);
+	use DijkstraStorageQueue;
 
 	-- Functions
 	function Step(l : Location; d: Direction) return Location 
@@ -194,9 +241,10 @@ package body solutionpk is
 		nl : Location;
 		td : Direction;
 		best_val : Integer := Integer'Last; 
+                hold_val : DijkstraStorage_Holder.Holder;
 		current_dij, next_dij: DijkstraStorage_Access;
 		traveled : DijkstraStorageSet.Set;
-		pendig_queue : DijkstraStorageSet.Set;
+		pendig_queue : DijkstraStorageQueue.Queue;
 		res : Integer := 0;
 	begin
 		grid_totals := CloneGrid(grid,0);
@@ -208,13 +256,14 @@ package body solutionpk is
 		current_dij.dirSteps := 0;
 		current_dij.d := None;
 
-		pendig_queue.Insert(current_dij);
+		pendig_queue.Enqueue(To_Holder(current_dij));
 
 		Put_Line("Main loop");
 		-- Main loop
-		while Length(pendig_queue) > 0 loop
-			current_dij := First_Element(pendig_queue);
-			Delete_First(pendig_queue);
+		loop
+                        pendig_queue.Dequeue(hold_val);
+                        current_dij := Element(hold_val);
+                        Clear(hold_val);
 			if current_dij.l.row = Integer(Length(grid)-1) and current_dij.l.col = Integer(Length(grid(0).all)-1) then
 				res := current_dij.cost;
 				Free(current_dij);
@@ -224,7 +273,7 @@ package body solutionpk is
 			if not Contains(traveled, current_dij) then
 				traveled.Insert(current_dij);
 				grid_totals(current_dij.l.row)(current_dij.l.col) := current_dij.cost;
-				Put_Line("Loc row: " & current_dij.l.row'Image & " col: " & current_dij.l.col'Image & " cost: " & current_dij.cost'Image);
+				--Put_Line("Loc row: " & current_dij.l.row'Image & " col: " & current_dij.l.col'Image & " cost: " & current_dij.cost'Image);
 				-- All other locations
 				for d in DirectionValid'First .. DirectionValid'Last loop
 					td := Opposite(d);
@@ -240,12 +289,8 @@ package body solutionpk is
 								next_dij.l.row := nl.row;
 								next_dij.l.col := nl.col;
 								next_dij.dirSteps := 1;
-								if not Contains(pendig_queue, next_dij) then
-									Put_Line("Nextdij dir: " & d'Image & " row: " & nl.row'Image & " col: " & nl.col'Image & " cost: " & next_dij.cost'Image);
-									pendig_queue.Insert(next_dij);
-								else
-									Free(next_dij);
-								end if;
+                                                                --Put_Line("Nextdij dir: " & d'Image & " row: " & nl.row'Image & " col: " & nl.col'Image & " cost: " & next_dij.cost'Image);
+                                                                pendig_queue.Enqueue(To_Holder(next_dij));
 							elsif d = current_dij.d and current_dij.dirSteps < LIMIT_STRAIGHT then
 								--Put_Line("Steps valid");
 								next_dij := new DijkstraStorage;
@@ -253,20 +298,15 @@ package body solutionpk is
 								next_dij.d := d;
 								next_dij.l.row := nl.row;
 								next_dij.l.col := nl.col;
-								next_dij.dirSteps := current_dij.dirSteps + 1;
-								if not Contains(pendig_queue, next_dij) then
-									Put_Line("Nextdij dir: " & d'Image & " row: " & nl.row'Image & " col: " & nl.col'Image & " cost: " & next_dij.cost'Image);
-									pendig_queue.Insert(next_dij);
-								else
-									Free(next_dij);
-								end if;
-
-							end if;
+                                                                next_dij.dirSteps := current_dij.dirSteps + 1;
+                                                                --Put_Line("Nextdij dir: " & d'Image & " row: " & nl.row'Image & " col: " & nl.col'Image & " cost: " & next_dij.cost'Image);
+                                                                pendig_queue.Enqueue(To_Holder(next_dij));
+                                                        end if;
 						end if;
 					end if;
 				end loop;
-				Put_Line("ResultPath");
-				PrintGrid(grid_totals);
+				--Put_Line("ResultPath");
+				--PrintGrid(grid_totals);
 			else
 				Free(current_dij);
 			end if;
@@ -279,12 +319,12 @@ package body solutionpk is
 			Free(current_dij);
 		end loop;
 
-		Put_Line("Pending queue: " & Length(pendig_queue)'Image);
-		while Length(pendig_queue) > 0 loop
-			current_dij := First_Element(pendig_queue);
-			Delete_First(pendig_queue);
-			Free(current_dij);
-		end loop;
+		--Put_Line("Pending queue: " & Length(pendig_queue)'Image);
+		--while Length(pendig_queue) > 0 loop
+		--	current_dij := First_Element(pendig_queue);
+		--	Delete_First(pendig_queue);
+		--	Free(current_dij);
+		--end loop;
 
 		Put_Line("ResultPath");
 		PrintGrid(grid_totals);
