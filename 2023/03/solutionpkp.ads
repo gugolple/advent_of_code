ed530c0b3b6fdf5cb8d73147ca4eb4670409559b
gugolple <juanignacio.croce@gmail.com>
2025-01-04 11:12:55 +0100

    Extended version working!

--- 2023/03/solutionpkp.ads
+++ 2023/03/solutionpkp.ads
@@ -1,4 +1,10 @@
+with Ada.Strings.Unbounded; use  Ada.Strings.Unbounded;
+with Ada.Containers.Ordered_Sets;
 package solutionpkp is
+	function replace_string(
+		source : in Unbounded_String;
+		value  : in String
+		) return Unbounded_String;
 	procedure Main;
 end solutionpkp;
 
