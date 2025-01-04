3420937e55e45ca7a5b9937c00a9ecd58312baf5
gugolple <juanignacio.croce@gmail.com>
2025-01-04 11:12:59 +0100

    2024 p5/normal
    Done, straight forwards, only check the invalidations

---
+++ 2024_python/p5/normal.py
@@ -0,0 +1,76 @@
+import unittest, sys, heapq, re, math
+
+def append_create(l, k, v):
+        if k not in l:
+            l[k] = set([v])
+        else:
+            l[k].add(v)
+
+def proc_rules(rules: str):
+    invalids_backwards = dict()
+    backwards = dict()
+    for entry in rules.split("\n"):
+        l,r = entry.split("|")
+        l = int(l)
+        r = int(r)
+        append_create(invalids_backwards, l, r)
+        append_create(backwards, r, l)
+    print(invalids_backwards)
+    print(backwards)
+    return invalids_backwards, backwards
+
+def check_oper(oper: str, invalids_backwards):
+    tot = 0
+    lr = [int(x) for x in oper.split(",")]
+    for i in range(1, len(lr)):
+        if lr[i] in invalids_backwards and invalids_backwards[lr[i]] & set(lr[:i]):
+            break
+    else:
+        mid = math.floor(len(lr)/2)
+        print(lr, len(lr), mid, lr[mid])
+        tot = lr[mid]
+    return tot
+
+def entry_func(inp: str):
+    tot = 0
+    rules, operations = inp.split("\n\n")
+    invalids_backwards, backwards = proc_rules(rules)
+    for oper in operations.split("\n"):
+        tot += check_oper(oper, invalids_backwards)
+    return tot
+
+if __name__ == "__main__":
+    inp_str = sys.stdin.read()
+    print(entry_func(inp_str[:-1]))
+
+class AllTestCase(unittest.TestCase):
+    def test_example(self):
+        inp_str = """47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47"""
+        self.assertEqual(entry_func(inp_str), 143)
