b9d28c1126af06be53c40b91ceaf440a173f4ee7
Gugolple <juanignacio.croce@gmail.com>
2025-11-10 12:50:07 +0100

    Reduced test for 04 and 04e

--- 2015/04.py
+++ 2015/04.py
@@ -4,12 +4,13 @@ import sys
 import hashlib
 #from pudb import set_trace; set_trace()
 
-def entry_func(inp_str) -> int:
+def entry_func(inp_str, tgt=5) -> int:
     inp_str = inp_str.strip()
     ansv = 0
+    tstr = ''.join("0"*tgt)
     while True:
         cans = hashlib.md5(bytes(str(inp_str + str(ansv)).encode("ascii"))).hexdigest()
-        if cans[:5] == "00000":
+        if cans[:tgt] == tstr:
             break
         ansv += 1
     return ansv
@@ -18,11 +19,11 @@ def entry_func(inp_str) -> int:
 class TestChallenge(unittest.TestCase):
     def test_basic(self):
         testPairs = [
-            ("abcdef", 609043),
-            ("pqrstuv", 1048970),
+            (("abcdef",1), 31),
+            (("pqrstuv",1), 53),
         ]
         for inp, res in testPairs:
-            self.assertEqual(entry_func(inp), res)
+            self.assertEqual(entry_func(*inp), res)
 
 if __name__ == '__main__':
     inp_str = sys.stdin.read()
